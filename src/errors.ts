8fcc1c2e37cf9c8cb870443741cdbb2715cd1223
невидимка <bessonov.do@perfema.com>
2022-04-14 18:35:00 +0300

    Add toString to DecodingException

--- src/errors.ts
+++ src/errors.ts
@@ -6,15 +6,19 @@ export class DecodingException {
         readonly garbage: unknown
     ) {}
 
-    print(warn: boolean = false) {
+    toString() {
         const path = this.path.filter(x => x !== undefined).map(x => typeof x === "string" ? x : `[${x}]`).join(".")
         const trace = this.scope.map(x => "\n\tat " + x).join("");
 
+        return `${this.message}\nValue path: ${path}\nScope trace:${trace}`;
+    }
+
+    print(warn: boolean = false) {
         if (warn) {
-            console.warn(`${this.message}\nValue path: ${path}\nScope trace:${trace}`);
+            console.warn(this.toString());
             console.warn("Garbage value:", this.garbage);
         } else {
-            console.error(`${this.message}\nValue path: ${path}\nScope trace:${trace}`);
+            console.error(this.toString());
             console.error("Garbage value:", this.garbage);
         }
     }
